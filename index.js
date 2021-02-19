cd3d6fb0b84c8075d856e69c840771cc44b5d4f8
Raquel Marques <araquelfmarques@gmail.com>
2021-02-19 20:58:52 +0000

    second commit

--- index.js
+++ index.js
@@ -2,4 +2,23 @@
 document.getElementById('myVideo').addEventListener('ended',myHandler,false);
 function myHandler(e) {
 $(".mensagem").show();
+$(".ciclos").hide();
+}
+
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
 }
