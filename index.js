1e932811b1cddac407cb148ac72561bcec3fb192
Raquel Marques <araquelfmarques@gmail.com>
2021-05-24 19:40:25 +0100

    fix convite3

--- index.js
+++ index.js
@@ -32,3 +32,31 @@ function myFunction() {
     btn.innerHTML = "Iniciar viagem";
   }
 }
+
+
+var 
+    myText = document.getElementById("bounceTxt").innerHTML,
+    wrapText = "";
+
+for (var i=0; i<myText.length; i++) {
+     wrapText += "<em>" + myText.charAt(i) + "</em>";
+}
+
+document.getElementById("bounceTxt").innerHTML = wrapText;
+
+var 
+    myLetters = document.getElementsByTagName("em"),
+    j = 0;
+
+function applyBounce() {
+     setTimeout(function() {
+          myLetters[j].className = "bounce-me";
+          j++;
+          
+          if (j < myLetters.length) {
+               applyBounce();
+          }
+     }, 250);
+}
+
+applyBounce();
